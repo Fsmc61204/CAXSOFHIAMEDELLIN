fc9d1519e88a08c676f6a37e86943c7850c6a636
Fsmc61204 <104523525+Fsmc61204@users.noreply.github.com>
2022-06-03 08:29:59 -0500

    Add files via upload

--- Python/primer trimestre/main.py
+++ Python/primer trimestre/main.py
@@ -1,27 +1,6 @@
-reprobados="9B"
-print("el grupo mas reprobado es",reprobados)
-
-name="Sofhi<3"
-print("my name is",name)
-
-favfood="pizza"
-print("my favourite food is",favfood)
-
-random="i'm the best louie"
-print("me?",random)
-
-group="9B"
-print("i'm in",group)
-
-name="Tommo<3"
-print("her name is",name)
-
-favfood="pizza"
-print("her favourite food is",favfood)
-
-random="she's the best billie stan"
-print("she?",random)
-
-group="9B"
-print("she's in",group)
-
+print("reprobados")
+print("dime como te llamas:")
+nombre=input()
+print("hola",nombre)
+edad=input("cual es tu edad")
+print("tienes", edad,"años")
